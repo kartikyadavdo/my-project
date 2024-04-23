ed6debd34156e8d74b27a1cb55415df27469b136
kartikyadavdo <kartik.yadav@dreamorbit.com>
2024-04-23 17:35:55 +0530

    added kanban ui

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -9,18 +9,9 @@ function Kanban() {
       <p className="text-3xl mb-8">Priority Board</p>
       <div className="flex flex-wrap h-[85vh] gap-10 ">
         <div className=" flex flex-col gap-4 flex-wrap  flex-1  border border-black">
-          {chartdata.map((x) => {
+          {chartdata.slice(0, 4).map((x) => {
             return (
               <>
-                {/* <div className="flex max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {x.date}
-                  </h5>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {x["The Pragmatic Engineer"]}
-                  </p>
-                </div> */}
-
                 <Card
                   className="mx-auto max-w-xs"
                   decoration="top"
@@ -32,14 +23,98 @@ function Kanban() {
                   <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
                     {x["The Pragmatic Engineer"]}
                   </p>
+                  <p className="text-sm dark:text-tremor-brand-faint text-dark-tremor-brand-faint">
+                    {" "}
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat vero tenetur quos? Dolorum vel rerum ratione eum est
+                    voluptate maiores?{" "}
+                  </p>
                 </Card>
               </>
             );
           })}
         </div>
-        <div className="flex flex-1  border border-black"></div>
-        <div className="flex flex-1  border border-black"></div>
-        <div className="flex flex-1  border border-black"></div>
+
+        <div className=" flex flex-col gap-4 flex-wrap  flex-1  border border-black">
+          {chartdata.slice(0, 3).map((x) => {
+            return (
+              <>
+                <Card
+                  className="mx-auto max-w-xs"
+                  decoration="top"
+                  decorationColor="red"
+                >
+                  <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                    {x.SemiAnalysis}
+                  </p>
+                  <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
+                    {x["The Pragmatic Engineer"]}
+                  </p>
+                  <p className="text-sm dark:text-tremor-brand-faint text-dark-tremor-brand-faint">
+                    {" "}
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat vero tenetur quos? Dolorum vel rerum ratione eum est
+                    voluptate maiores?{" "}
+                  </p>
+                </Card>
+              </>
+            );
+          })}
+        </div>
+
+        <div className=" flex flex-col gap-4 flex-wrap  flex-1  border border-black">
+          {chartdata.slice(0, 2).map((x) => {
+            return (
+              <>
+                <Card
+                  className="mx-auto max-w-xs"
+                  decoration="top"
+                  decorationColor="yellow"
+                >
+                  <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                    {x.SemiAnalysis}
+                  </p>
+                  <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
+                    {x["The Pragmatic Engineer"]}
+                  </p>
+                  <p className="text-sm dark:text-tremor-brand-faint text-dark-tremor-brand-faint">
+                    {" "}
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat vero tenetur quos? Dolorum vel rerum ratione eum est
+                    voluptate maiores?{" "}
+                  </p>
+                </Card>
+              </>
+            );
+          })}
+        </div>
+
+        <div className="flex flex-col gap-4 flex-wrap  flex-1  border border-black">
+          {chartdata.slice(0, 4).map((x) => {
+            return (
+              <>
+                <Card
+                  className="mx-auto max-w-xs"
+                  decoration="top"
+                  decorationColor="violet"
+                >
+                  <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                    {x.SemiAnalysis}
+                  </p>
+                  <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
+                    {x["The Pragmatic Engineer"]}
+                  </p>
+                  <p className="text-sm dark:text-tremor-brand-faint text-dark-tremor-brand-faint">
+                    {" "}
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat vero tenetur quos? Dolorum vel rerum ratione eum est
+                    voluptate maiores?{" "}
+                  </p>
+                </Card>
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
